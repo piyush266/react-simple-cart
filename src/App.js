982c81b5a71c1204f338f1557a0491eb8789caee
Piyush <piyushraghav12@gmail.com>
2022-08-31 15:53:48 +0530

    Reverting last 2 commits

--- src/App.js
+++ src/App.js
@@ -39,7 +39,6 @@ export default function App() {
 
   return (
     <>
-      <div className="hello"></div>
       <div style={{marginLeft: "14px", marginRight: "60px"}}>
         <input style={{width: "80%", padding: "10px"}} type="search" placeholder="Search Product" onChange={(e) => searchProduct(e.target.value)}></input>
       </div>
